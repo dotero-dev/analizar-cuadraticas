db7827fd7590ca2da631c85cf2976ddb2b573620
dotero <doterodev@gmail.com>
2024-04-02 12:28:18 +0200

    Script ready

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -6,7 +6,9 @@ export default {
 			'principal-purple': "#ac3973",
 			'principal-white': "#f5f5f5",
 			'principal-black': "#111111",
-			'principal-blue': "#005580"
+			'principal-blue': "#005580",
+			'principal-cyan': "#52527a",
+			'principal-purple-two': "#732673",
 		}
 	},
 	plugins: [],
